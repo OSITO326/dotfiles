1c5e44bdce1cea1592b1c448b16cf1ae88b12751
Kevin <326.osito@gmail.com>
2021-10-28 02:51:17 -0400

    Update bspwm, sxhkd and aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -9,18 +9,26 @@ alias ~="cd ~"
 alias dotfiles='cd $DOTFILES_PATH'
 
 # Git
-alias gaa="git add -A"
-alias gc='$DOTLY_PATH/bin/dot git commit'
-alias gca="git add --all && git commit --amend --no-edit"
-alias gco="git checkout"
-alias gd='$DOTLY_PATH/bin/dot git pretty-diff'
-alias gs="git status -sb"
-alias gf="git fetch --all -p"
-alias gps="git push"
-alias gpsf="git push --force"
-alias gpl="git pull --rebase --autostash"
+alias gaa="git add --all"
+alias gcm="git commit -m"
+alias gst="git status"
+alias gpl="git pull"
+alias gc="git clone"
 alias gb="git branch"
-alias gl='$DOTLY_PATH/bin/dot git pretty-log'
+alias gco="git checkout"
+
+#alias gaa="git add -A"
+#alias gc='$DOTLY_PATH/bin/dot git commit'
+#alias gca="git add --all && git commit --amend --no-edit"
+#alias gco="git checkout"
+#alias gd='$DOTLY_PATH/bin/dot git pretty-diff'
+#alias gs="git status -sb"
+#alias gf="git fetch --all -p"
+#alias gps="git push"
+#alias gpsf="git push --force"
+#alias gpl="git pull --rebase --autostash"
+#alias gb="git branch"
+#alias gl='$DOTLY_PATH/bin/dot git pretty-log'
 
 #NeoVim
 alias v='nvim'
