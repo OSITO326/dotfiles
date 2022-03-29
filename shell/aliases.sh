ce0048422826a1e0e95120ea5f869959a8a2a09a
Kevin <326.osito@gmail.com>
2022-03-29 01:23:55 -0400

    add alias youtube-dl and starship on zshrc

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -49,11 +49,22 @@ alias html='cd /usr/share/nginx/html/'
 
 #Bat
 #alias cat='bat'
-alias cat='bat --style=plain --paging=never'
+alias bat='bat --style=plain --paging=never'
 
 #Arch/Manjaro
 #alias update='sudo pacman -Sy'
 #alias upgrade='sudo pacman -Syu'
+
+# youtube-dl
+alias yta-aac="youtube-dl --extract-audio --audio-format aac "
+alias yta-best="youtube-dl --extract-audio --audio-format best "
+alias yta-flac="youtube-dl --extract-audio --audio-format flac "
+alias yta-m4a="youtube-dl --extract-audio --audio-format m4a "
+alias yta-mp3="youtube-dl --extract-audio --audio-format mp3 "
+alias yta-opus="youtube-dl --extract-audio --audio-format opus "
+alias yta-vorbis="youtube-dl --extract-audio --audio-format vorbis "
+alias yta-wav="youtube-dl --extract-audio --audio-format wav "
+alias ytv-best="youtube-dl -f bestvideo+bestaudio "
 
 # Utils
 alias k='kill -9'
